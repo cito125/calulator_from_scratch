adba9cb144e26eb463f11e1ed29bc5c2d1f6942a
Max Rosado <max.a.rosado@gmail.com>
2016-10-14 22:27:23 -0400

    Added trig functions, factorials, pi, euler's number to operateable parameters of the calculator

--- src/nyc/c4q/Main.java
+++ src/nyc/c4q/Main.java
@@ -1,9 +1,10 @@
 package nyc.c4q;
 
+import sun.jvm.hotspot.oops.Array;
 import sun.jvm.hotspot.types.basic.BasicNarrowOopField;
 
 import java.math.BigDecimal;
-import java.util.DoubleSummaryStatistics;
+import java.util.*;
 import java.util.function.DoubleBinaryOperator;
 import java.util.regex.Matcher;
 
@@ -13,24 +14,15 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Calculator cal = new Calculator();
-//        System.out.println(makeNice("4+4*2*(3)2(6)"));
-//        System.out.println(calculate(makeNice("4+4*2*(3)2(6)")));
-//        System.out.println(calculate(makeNice("4(4+4*2*(3)*2*(6))")));
-//        System.out.println(calculate(makeNice("4+4.5")));
-//        System.out.println(calculate(makeNice("4+3(2")));
-        //System.out.println(Math.pow(.01,4));
-        //System.out.println(calculate(".01^4"));
-        //System.out.println(calculate("-3"));
-        //System.out.println(Double.parseDouble(""));
-//        double num = -3;
-//        System.out.println(-3.0+((-num)));
-        System.out.println(arcCosine("1","false"));
-//        System.out.println(subtract("3","4","true","true"));
-//        String[] myG = getOperandsArray("4-5+2","+");
-//        for (int i = 0; i < myG.length; i++) {
-//            System.out.println(myG[i]);
+//        String[] yo = "dang bro ( (".split(" ");
+//        List<String> b = new ArrayList<>(Arrays.asList(yo));
+//        System.out.println(b);
+//        System.out.println(b.removeAll(Collections.singleton("(")));
+//        System.out.println(b);
+//            String[] yo = getFactArray("3+2!");
+//        for (int i = 0; i < yo.length; i++) {
+//            System.out.println(yo[i]);
 //        }
-
 
     }
 
@@ -148,6 +140,25 @@ public class Main {
         return Double.toString(result);
     }
 
+    static String factorial(String a){
+        Double numa = Double.parseDouble(a);
+        double result = mathFact(numa);
+        return Double.toString(result);
+    }
+
+    static Double mathFact(Double a) {
+        if (a <= 0) {
+            if (a == Math.round(a)) {
+                return 1.0;
+
+            }else{
+                return Math.sqrt(3.14159265359);
+            }
+        }
+
+        return a * mathFact(a - 1);
+    }
+
     static String sine(String a, String isANegative){
         double numa = Double.parseDouble(a);
         boolean isANeg = Boolean.valueOf(isANegative);
@@ -209,23 +220,16 @@ public class Main {
         return Double.toString(result);
     }
 
-    static String pI(String isANegative){
+    static String pI(){
         double result = 3.14159265359;
-        boolean isANeg = Boolean.valueOf(isANegative);
-        if(isANeg){
-            result = result * -1;
-        }
         return Double.toString(result);
     }
 
-    static String euler(String isANegative){
+    static String euler(){
         double result = 2.71828182846;
-        boolean isANeg = Boolean.valueOf(isANegative);
-        if(isANeg){
-            result = result * -1;
-        }
         return Double.toString(result);
     }
+
 
     static String calculate(String inputString) {
         String exponent = "^";
@@ -245,25 +249,104 @@ public class Main {
         String e = "e";
         String exp = "E";
         String fact = "!";
-
-        if (inputString.contains(openParentheses)){
-            if(inputString.contains(closeParentheses)){
+        if (inputString.contains(pi)) {
+            String operatedString = inputString.replaceAll(pi, pI());
+            return calculate(operatedString);
+        }
+        if (inputString.contains(e)) {
+            String operatedString = inputString.replaceAll(e, euler());
+            return calculate(operatedString);
+        }
+        if (inputString.contains(openParentheses)) {
+            if (inputString.contains(closeParentheses)) {
                 String toReplace = "(" + flattenParenthese(inputString) + ")";
                 String replacement = calculate(flattenParenthese(inputString));
-                String operatedString = inputString.replace(toReplace,replacement);
+                String operatedString = inputString.replace(toReplace, replacement);
                 return calculate(operatedString);
-            }else{
+            } else {
                 String toReplace = "(" + flattenParenthese(inputString);
                 String replacement = calculate(flattenParenthese(inputString));
-                String operatedString = inputString.replace(toReplace,replacement);
+                String operatedString = inputString.replace(toReplace, replacement);
                 return calculate(operatedString);
             }
+        }else if(inputString.contains(fact)){
+            String[] thisCalculation = getFactArray(inputString);
+            int start = Integer.parseInt(thisCalculation[1]);
+            int end = Integer.parseInt(thisCalculation[2]);
+            String toReplace = inputString.substring(start,end) + "!";
+            String replacement = factorial(thisCalculation[0]);
+            String operatedString = inputString.replace(toReplace,replacement);
+            return calculate(operatedString);
+
+        }else if(inputString.contains(sin)){
+            String [] thisCalculation = getTrigOpearndArray(inputString,"n");
+            int start = Integer.parseInt(thisCalculation[1]);
+            int end = Integer.parseInt(thisCalculation[2]);
+            String toReplace = sin + inputString.substring(start,end);
+            String replacement = sine(thisCalculation[0],thisCalculation[3]);
+            String operatedString = inputString.replace(toReplace,replacement);
+            return calculate(operatedString);
+
+        }else if(inputString.contains(cos)){
+            String [] thisCalculation = getTrigOpearndArray(inputString,"n");
+            int start = Integer.parseInt(thisCalculation[1]);
+            int end = Integer.parseInt(thisCalculation[2]);
+            String toReplace = cos + inputString.substring(start,end);
+            String replacement = cosine(thisCalculation[0],thisCalculation[3]);
+            String operatedString = inputString.replace(toReplace,replacement);
+            return calculate(operatedString);
+
+        }else if(inputString.contains(tan)){
+            String [] thisCalculation = getTrigOpearndArray(inputString,"n");
+            int start = Integer.parseInt(thisCalculation[1]);
+            int end = Integer.parseInt(thisCalculation[2]);
+            String toReplace = tan + inputString.substring(start,end);
+            String replacement = tangent(thisCalculation[0],thisCalculation[3]);
+            String operatedString = inputString.replace(toReplace,replacement);
+            return calculate(operatedString);
+
+        }else if(inputString.contains(arcSin)){
+            String [] thisCalculation = getTrigOpearndArray(inputString,"n");
+            int start = Integer.parseInt(thisCalculation[1]);
+            int end = Integer.parseInt(thisCalculation[2]);
+            String toReplace = arcSin + inputString.substring(start,end);
+            String replacement = arcSine(thisCalculation[0],thisCalculation[3]);
+            String operatedString = inputString.replace(toReplace,replacement);
+            return calculate(operatedString);
+
+        }else if(inputString.contains(arcCos)){
+            String [] thisCalculation = getTrigOpearndArray(inputString,"n");
+            int start = Integer.parseInt(thisCalculation[1]);
+            int end = Integer.parseInt(thisCalculation[2]);
+            String toReplace = arcCos + inputString.substring(start,end);
+            String replacement = arcCosine(thisCalculation[0],thisCalculation[3]);
+            String operatedString = inputString.replace(toReplace,replacement);
+            return calculate(operatedString);
+
+        }else if(inputString.contains(arcTan)){
+            String [] thisCalculation = getTrigOpearndArray(inputString,"n");
+            int start = Integer.parseInt(thisCalculation[1]);
+            int end = Integer.parseInt(thisCalculation[2]);
+            String toReplace = arcTan + inputString.substring(start,end);
+            String replacement = arcTangent(thisCalculation[0],thisCalculation[3]);
+            String operatedString = inputString.replace(toReplace,replacement);
+            return calculate(operatedString);
+
         }else if(inputString.contains(exponent)){
             String[] thisCalculation = getOperandsArray(inputString,exponent);
             int start = Integer.parseInt(thisCalculation[2]);
             int end = Integer.parseInt(thisCalculation[3]);
             String toReplace = inputString.substring(start,end);
             String replacement = exponent(thisCalculation[0],thisCalculation[1],thisCalculation[4],thisCalculation[5]);
+            String operatedString = inputString.replace(toReplace,replacement);
+            return calculate(operatedString);
+
+        }else if(inputString.contains(exp)){
+            String[] thisCalculation = getOperandsArray(inputString,exp);
+            int start = Integer.parseInt(thisCalculation[2]);
+            int end = Integer.parseInt(thisCalculation[3]);
+            String toReplace = inputString.substring(start,end);
+            String replacement = exp(thisCalculation[0],thisCalculation[1],thisCalculation[4],thisCalculation[5]);
             String operatedString = inputString.replace(toReplace,replacement);
             return calculate(operatedString);
 
@@ -311,7 +394,7 @@ public class Main {
     private static boolean isNegNum(String inputString){
         if(inputString.charAt(0) == '-'){
             String searchForOperand = inputString.substring(1,inputString.length());
-            if(hasOperand(searchForOperand)){
+            if(hasOperand(searchForOperand) || hasTrigOperand(searchForOperand)){
                 return false;
             }else{
                 return true;
@@ -340,11 +423,72 @@ public class Main {
 
     static boolean hasOperand(String substring){
         if(substring.contains("^") || substring.contains("*") || substring.contains("/") || substring.contains("+")
-                || substring.contains("-") ){
+                || substring.contains("-") || substring.contains("E") || substring.contains("!")){
             return true;
         }
         return false;
     }
+
+    static boolean hasTrigOperand(String substring){
+        if( substring.contains("n") || substring.contains("a") ||substring.contains("s") || substring.contains("c")
+                || substring.contains("t")){
+            return true;
+        }
+        return false;
+    }
+
+    static String[] getFactArray(String inputString){
+        String[] operationArray = new String[3];
+        int end = inputString.indexOf("!");
+        int start = 0;
+        for (int i = end; i > 0 ; i--) {
+            if(hasOperand(inputString.substring(i ,end))
+               || hasTrigOperand(inputString.substring(i ,end))
+               ){
+                    start = i +1;
+                    break;
+                }
+
+            }
+        operationArray[0] = inputString.substring(start,end);
+        operationArray[1] = Integer.toString(start);
+        operationArray[2] = Integer.toString(end);
+
+        return operationArray;
+    }
+
+    static String[] getTrigOpearndArray(String inputString, String operator){
+        String[] operationArray = new String[4];
+        int operatorIndex = inputString.indexOf(operator);
+        boolean isNumberNegative = false;
+        int whereToStartSecondLoop = 1;
+        if(inputString.charAt(operatorIndex + 1) == '-'){
+            isNumberNegative = true;
+            whereToStartSecondLoop = 2;
+        }
+
+        int start = operatorIndex + whereToStartSecondLoop;
+        int endExclusive = 0;
+
+        for (int i = operatorIndex + whereToStartSecondLoop ; i < inputString.length() ; i++) {
+            if(hasOperand(inputString.substring(operatorIndex + whereToStartSecondLoop ,i))
+                    || hasTrigOperand(inputString.substring(operatorIndex + whereToStartSecondLoop ,i))
+                    ){
+                endExclusive = i - 1;
+                break;
+            } else if( i == inputString.length() - 1){
+                endExclusive = inputString.length();
+            }
+        }
+        operationArray[0] = inputString.substring(start, endExclusive);
+        operationArray[1] = Integer.toString(operatorIndex + 1);
+        operationArray[2] = Integer.toString(endExclusive);
+        operationArray[3] = Boolean.toString(isNumberNegative);
+
+
+        return operationArray;
+    }
+
 
     static String[] getOperandsArray(String inputString, String operator){
         String[] operationArray = new String[6];
@@ -365,7 +509,7 @@ public class Main {
             if(i == 0) {
                 start = 0;
                 break;
-            }else if (hasOperand(inputString.substring(i, operatorIndex))) {
+            }else if (hasOperand(inputString.substring(i, operatorIndex)) || hasTrigOperand(inputString.substring(i,operatorIndex))) {
                 if(inputString.charAt(i) == '-' ){
                     if(i == 0){
                         isFirstNumberNegative = true;
@@ -392,7 +536,9 @@ public class Main {
         }
 
         for (int i = operatorIndex + whereToStartSecondLoop ; i < inputString.length() ; i++) {
-            if(hasOperand(inputString.substring(operatorIndex + whereToStartSecondLoop ,i))){
+            if(hasOperand(inputString.substring(operatorIndex + whereToStartSecondLoop ,i))
+             || hasTrigOperand(inputString.substring(operatorIndex + whereToStartSecondLoop ,i))
+                    ){
                 endExclusive = i - 1;
                 break;
             } else if( i == inputString.length() - 1){
